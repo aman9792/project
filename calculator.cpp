ddbaaac590c531a9d4db7b33fd52a7eb2d55308f
aman9792 <56406069+aman9792@users.noreply.github.com>
2020-01-25 14:14:25 +0530

    Created calulator

---
+++ calculator.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+char c;
+cout<<"Enter two numbers:";
+cin>>a>>b;
+cout<<"Enter the operator";
+cin>>c;
+switch('c')
+{
+case '+':cout<<"Sum:"<<pass;
+break;
+
+case '-':cout<<"Sum:"<<pass;
+break;
+
+case '*':cout<<"Sum:"<<pass;
+break;
+
+case '/':cout<<"Sum:"<<pass;
+break;
+default:cout<<"Wrong operator";
+break;
+}
+}
